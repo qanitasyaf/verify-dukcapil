e13e2930f5dca613d5358b8172f5cc5a30fcb4ed
qanitasyaf <qanitafiqa@gmail.com>
2025-07-16 11:25:12 +0700

    Bismillah

--- src/test/java/com/dukcapil/dukcapil/DukcapilApplicationTests.java
+++ src/test/java/com/dukcapil/dukcapil/DukcapilApplicationTests.java
@@ -2,12 +2,11 @@ package com.dukcapil.dukcapil;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.dukcapil.service.service.DukcapilService;
 
-@SpringBootTest
-class DukcapilApplicationTests {
-
+@SpringBootTest(classes = DukcapilService.class)
+public class DukcapilApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
